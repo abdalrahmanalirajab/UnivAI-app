167d713134b8ace1662cfd37e64359c83da18569
abdelrhman <moaliabody@gmail.com>
2026-07-27 02:36:47 +0300

    Build PromoVideo section with 16:9 placeholder facade and VIDEO config

--- app/components/landing/PromoVideo.tsx
+++ app/components/landing/PromoVideo.tsx
@@ -3,6 +3,7 @@
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import PlayArrow from "@mui/icons-material/PlayArrow";
@@ -11,6 +12,7 @@ import content from "./content";
 const VIDEO = {
   src: null as string | null,
   poster: null as string | null,
+  caption: "Promo video coming soon.",
   provider: "self-hosted" as "self-hosted" | "embed",
 };
 
@@ -25,20 +27,18 @@ export default function PromoVideo() {
             {promoVideo.heading}
           </Typography>
           <Paper variant="outlined">
-            <Typography align="center">
-              {VIDEO.src ? (
-                null
-              ) : (
+            <Box sx={{ aspectRatio: "16 / 9", display: "flex", alignItems: "center", justifyContent: "center" }}>
+              {VIDEO.src ? null : (
                 <IconButton aria-label={promoVideo.playAriaLabel}>
                   <PlayArrow />
                 </IconButton>
               )}
-            </Typography>
+              {/* Reserved: captions / transcript link */}
+            </Box>
           </Paper>
           <Typography variant="body2" color="text.secondary">
             {promoVideo.caption}
           </Typography>
-          {/* Reserved: captions / transcript link */}
         </Stack>
       </Container>
     </section>
